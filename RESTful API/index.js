88c25c3bd3ba3ac74565491763b13a6325d03132
Sofie Tosti Madsen <sotm@itu.dk>
2022-04-08 10:19:31 +0200

    dw

--- RESTful API/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-/*LISTENING TO GET REQUEST - dynamic port variable*/ 
-const port = process.env.PORT || 3000; //if no port is given, use 3000
-const app = express(); 
-
-app.get("/", (req, res) => res.send("Server 1: Hello from index.js"));
-
-app.listen(port, () => console.log(`listening on ${port}`));
