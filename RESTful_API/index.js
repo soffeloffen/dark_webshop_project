4546264cec3c2938e0f8055c0e968540b254a5eb
Sofie Tosti Madsen <sotm@itu.dk>
2022-04-08 10:42:33 +0200

    cd

---
+++ RESTful_API/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000; //if no port is given choose 3000
+
+app.get("/", (req, res) => res.send("hey yo"));
+app.listen(port, () => console.log(`listening on ${port}`));
+
+
