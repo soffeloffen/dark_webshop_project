943eeb6c783034038bd9b3d6112821ac44da329c
anso <asod@itu.dk>
2022-04-17 14:54:56 +0200

    endelig

---
+++ RESTful_API/webshop/js/basketStats.js
@@ -0,0 +1,16 @@
+var amount= 0;
+
+function setDrug1(){
+localStorage.setItem( "Drug1Name", "Magic Mushrooms" );
+localStorage.setItem( "Drug1Amount", 1);
+}
+
+function updateDrug1(){
+var total = parseInt(localStorage.getItem( "Drug1Amount" ) );
+var updatedTotal = total +1;
+localStorage.setItem( "Drug1Amount", updatedTotal ); 
+}
+
+function reset() {
+    sessionStorage.removeItem;
+}
