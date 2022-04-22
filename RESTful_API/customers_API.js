8ba53326c54724aafc2a3725aeb79f883cf9c7e2
Sofie Tosti Madsen <sotm@itu.dk>
2022-04-22 12:40:55 +0200

    mkj

--- RESTful_API/customers_API.js
+++ RESTful_API/customers_API.js
@@ -4,11 +4,11 @@ const app2 = express();
 app2.use(cors());
 const port = process.env.PORT || 3000; //if no port is given choose 3000
 
-let Allproducts = require("./products/products.json");
 const { type } = require("express/lib/response");
 const { all } = require("express/lib/application");
 app2.use(express.static('webshop'))
-app2.get("/", (req, res) => res.send("Server_2: Products"));
+app2.get("/", (req, res) => res.send("Server_3: customers"));
+let customers = require("./customers/customers.json");
 
 app2.listen(port, function (err) {
   if (err) console.log("error in server");
@@ -17,7 +17,8 @@ app2.listen(port, function (err) {
 
 app2.get("/", (req, res) => res.send("Server_3: Customers"));
 
-//Get all customers
+//get customers 
 app2.get("/customers", (req, res) => {
-  res.send(req.params);
+  res.send(customers);
 });
+
